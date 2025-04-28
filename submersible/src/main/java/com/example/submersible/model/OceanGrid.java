176dca3d6cf2d1ea09e838b722aec056ec8409aa
PoojaKachroo <poojakachroo2311@gmail.com>
2025-04-28 20:24:55 +0530

    Added class for ocean grid functionality

---
+++ submersible/src/main/java/com/example/submersible/model/OceanGrid.java
@@ -0,0 +1,29 @@
+package com.example.submersible.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class OceanGrid {
+
+    private final int width;
+    private final int height;
+    private final Set<String> obstacles = new HashSet<>();
+
+    public OceanGrid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean isInsideGrid(int x, int y) {
+        return x >= 0 && y >= 0 && x < width && y < height;
+    }
+
+    public boolean hasObstacle(int x, int y) {
+        return obstacles.contains(x + "," + y);
+    }
+
+    public void addObstacle(int x, int y) {
+        obstacles.add(x + "," + y);
+    }
+
+}
