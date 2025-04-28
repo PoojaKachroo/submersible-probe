5ad9d8d3f07cf916da22df5c3f1bb23ff62d9301
PoojaKachroo <poojakachroo2311@gmail.com>
2025-04-28 20:04:04 +0530

    Tests for direction functionality

---
+++ submersible/src/test/java/com/example/submersible/model/DirectionTest.java
@@ -0,0 +1,28 @@
+package com.example.submersible.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DirectionTest {
+
+    @Test
+    void testTurnLeftFromNorth() {
+        assertEquals(Direction.WEST, Direction.NORTH.left());
+    }
+
+    @Test
+    void testTurnLeftFromSouth() {
+        assertEquals(Direction.EAST, Direction.SOUTH.turnLeft());
+    }
+
+    @Test
+    void testTurnRightFromEast() {
+        assertEquals(Direction.SOUTH, Direction.EAST.turnRight());
+    }
+
+    @Test
+    void testTurnRightFromNorth() {
+        assertEquals(Direction.EAST, Direction.NORTH.turnRight());
+    }
+}
